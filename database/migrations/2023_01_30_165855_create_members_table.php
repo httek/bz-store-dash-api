9f0833675b47f3a2300d02edef6d5bb62a6e8da3
enhe <enhe@iluckin.cn>
2023-01-31 09:31:35 +0800

    fi

---
+++ database/migrations/2023_01_30_165855_create_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60)->nullable();
+            $table->string('avatar', 400)->nullable();
+            $table->string('mobile', 11)->nullable()->index();
+            $table->string('password', 400)->nullable();
+            $table->char('openid', 50)->nullable()->index();
+            $table->char('unionid', 50)->nullable()->index();
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
